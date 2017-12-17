80ac5199971a89098f7b3e9fc28489561e29ccac
Théo FIDRY <theo.fidry@gmail.com>
2017-12-17 19:24:57 +0000

    Fix test script

--- tests/sub.php
+++ tests/sub.php
@@ -11,5 +11,6 @@
 
 require __DIR__.'/../fixtures/vendor-bin/sub/vendor/autoload.php';
 
-\Acme\Application::salute().PHP_EOL;
-\Acme\DevApplication::salute().PHP_EOL;
+\Acme\Application::salute();
+echo PHP_EOL;
+\Acme\DevApplication::salute();
